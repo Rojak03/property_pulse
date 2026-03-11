0be0f9b781fe06bbc43cc183b83f9251a8d06f8c
Rojak03 <chrisnj.ftw@gmail.com>
2026-03-11 11:40:31 +0100

    removed icon from login button on moblie menu

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -216,8 +216,6 @@ const Navbar = () => {
             )}
             {!isLoggedIn && (
               <button className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-4">
-                <FaGoogle className="text-white mr-2" />
-
                 <span>Login or Register</span>
               </button>
             )}
