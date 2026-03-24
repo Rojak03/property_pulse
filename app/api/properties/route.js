2b5b953a3c49ecd55e1311fcc7be76bd671b2244
Rojak03 <chrisnj.ftw@gmail.com>
2026-03-24 10:35:20 +0100

    created add property POST object for database

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -17,3 +17,45 @@ export const GET = async (request) => {
     });
   }
 };
+
+//POST /api/properties
+export const POST = async (request) => {
+  try {
+    const formData = await request.formData();
+    // access all vaules from amneities and images
+    const amenities = formData.getAll("amenities");
+    const images = formData.getAll(images).filter((image) => image.name !== "");
+    //Create property Data object for database
+    const propertyData = {
+      type: formData.get("type"),
+      name: formData.get("name"),
+      description: formData.get("description"),
+      location: {
+        street: formData.get("location.street"),
+        city: formData.get("location.city"),
+        state: formData.get("location.state"),
+        zipcode: formData.get("location.zipcode"),
+      },
+      beds: formData.get("beds"),
+      baths: formData.get("baths"),
+      square_feet: formData.get("square_feet"),
+      amenities,
+      rates: {
+        weekly: formData.get("rates.weekly"),
+        monthly: formData.get("rates.monthly"),
+        nightly: formData.get("rates.nightly."),
+      },
+      seller_info: {
+        name: formData.get("seller_info.name"),
+        email: formData.get("seller_info.email"),
+        phone: formData.get("seller_info.phone"),
+      },
+      owner: userId,
+    };
+    await connectDB;
+  } catch (error) {
+    return new Response("Failed to post property", {
+      stauts: 500,
+    });
+  }
+};
