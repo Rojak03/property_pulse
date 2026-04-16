91fd0a51c13ac3c89cfd96c9bfd644a3be365278
Rojak03 <chrisnj.ftw@gmail.com>
2026-04-16 13:44:57 +0200

    added react share package and created share buttons for properties

--- app/api/bookmarks/route.js
+++ app/api/bookmarks/route.js
@@ -4,7 +4,31 @@ import Property from "@/models/Property";
 import { getSessionuser } from "@/utils/getSessionUser";
 
 export const dyanmic = "force-dynamic";
+//GET /api/bookmarks
+export const GET = async () => {
+  try {
+    await connectDB();
 
+    const sessionUser = await getSessionuser();
+
+    if (!sessionUser || !sessionUser.userId) {
+      return new Response("user Id is requried", { status: 404 });
+    }
+
+    const { userId } = sessionUser;
+
+    //Find user in database
+    const user = await User.findOne({ _id: userId });
+
+    //Get user bookmarks
+    const bookmarks = await Property.find({ _id: { $in: user.bookmarks } });
+
+    return new Response(JSON.stringify(bookmarks), { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return new Response("Something went wrong", { status: 500 });
+  }
+};
 export const POST = async (request) => {
   try {
     await connectDB();
